36ad44cb65940f9b9e65d4cf2b9900e26301da31
Jesús Rosendo Castañeda Villarreal <practicascomer1@ragasa.com.mx>
2019-01-23 16:12:44 -0600

    crear endpoint para probar webhook 2

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.post("/webhook", (req, res) => {
     console.log("SE RECIBIO PETICION A WEBHOOK")
     console.log(`req ${req}`)
     console.log(`res ${res}`)
-    res.json({respuesta: "WEBHOOK", fecha: new Date().toISOString(), req: req, res: res})
+    res.json({respuesta: "WEBHOOK", fecha: new Date().toISOString(), req: req})
 })
 
 app.get("/", (req, res) => {
